27f735b215f39bfce361302d99c922827c022f0e
Duy <81013045+legendzero456789@users.noreply.github.com>
2022-12-26 18:56:44 +0700

    a

--- Backend/SQLQuery1.sql
+++ Backend/SQLQuery1.sql
@@ -75,13 +75,22 @@ create table CartInfo(
 	foreign key (product_id) references Product(id)
 )
 
+create table Rate(
+	user_id varchar(5),
+	product_id varchar(10),
+	score float default(0),
+
+	foreign key (user_id) references Users(id),
+	foreign key (product_id) references Product(id)
+)
+
 
 insert into Roles values('00001', 'admin')
 insert into Roles values('00002', 'customer')
 
 insert into Users values('aaaaa','admin','$2a$12$68/ysYveNCsqjD2ocSxSSe7LeEusqEbgJjyiVPmYszdCxP8.oaWFG','00002',N'Nguyễn Hoàng Thái Dương','0366330205',N'30 đường Lê Hồng Phong',N'Hồ Chí Minh',N'Bình Tân',N'Bình trị Đông B', 1)
 insert into Users values('aaaad','admin@gmail.com','$2a$12$68/ysYveNCsqjD2ocSxSSe7LeEusqEbgJjyiVPmYszdCxP8.oaWFG','00001',N'Nguyễn Hoàng Thái Dương','0366330205',N'481 Tỉnh Lộ 10',N'Thành phố Hồ Chí Minh',N'Quận Bình Tân',N'Phường Bình Trị Đông B', 1)
-
+insert into Users values('bbbbb','customer','$2a$12$68/ysYveNCsqjD2ocSxSSe7LeEusqEbgJjyiVPmYszdCxP8.oaWFG','00002',N'Nguyễn Hoàng Thái Dương','0366330205',N'30 đường Lê Hồng Phong',N'Hồ Chí Minh',N'Bình Tân',N'Bình trị Đông B', 1)
 
 
 update Users set password = '$2a$12$du28zFKD4Zv0b4yRcM6f5.BThFPqCk7OhZhoOpe2nS3optbQ1aaWi' where id = 'aaaaa'
